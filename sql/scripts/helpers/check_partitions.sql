fef435d9595c2cebb86422f2a116f4a4d32bc51f
yoni <jonny@edennetwork.io>
2024-05-16 09:07:31 +0200

    added new readme. removed sql and deployment folder as most pertinent info is now including in the readmes

--- sql/scripts/helpers/check_partitions.sql
+++
@@ -1,7 +0,0 @@
-select  partition_id, 
-      total_rows
-from `eden-data-private.mev_boost.INFORMATION_SCHEMA.PARTITIONS`
-where table_name = "bids"
-  and partition_id != "__NULL__"
-  and partition_id is not null
-order by partition_id desc ;
--- sql/scripts/helpers/find_block_timestamp_for_slot_range.sql
+++
@@ -1,7 +0,0 @@
-select min(slot), max(slot)
-from `enduring-art-207419.mev_boost.bids_staging` 
-
-select block_slot, block_timestamp
-from `public-data-finance.crypto_ethereum2.beacon_blocks`
-where block_timestamp > timestamp("2023-12-25")
-  and block_slot in (8151602, 8158048)
